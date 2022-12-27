fa350b9df6e07641ae2fdd7cc8a161c1b7027ae2
BalcarKMPL <mazur.jakub05@gmail.com>
2022-12-27 23:53:10 +0100

    probably working negation parsing

--- main.cpp
+++ main.cpp
@@ -18,42 +18,42 @@ struct Variable : Formula {
 };
 
 struct Implication : Formula {
-    Formula& c1; Formula& c2;
-    Implication(Formula& c1, Formula& c2): c1(c1), c2(c2) {}
+    Formula* c1; Formula* c2;
+    Implication(Formula* c1, Formula* c2): c1(c1), c2(c2) {}
     bool val() override {
-        return !c1 || c2;
+        return !c1->val() || c2->val();
     }
 };
 
 struct Alternative : Formula {
-    Formula& c1; Formula& c2;
-    Alternative(Formula& c1, Formula& c2): c1(c1), c2(c2) {}
+    Formula* c1; Formula* c2;
+    Alternative(Formula* c1, Formula* c2): c1(c1), c2(c2) {}
     bool val() override {
-        return c1 || c2;
+        return c1->val() || c2->val();
     }
 };
 
 struct Conjunction : Formula {
-    Formula& c1; Formula& c2;
-    Conjunction(Formula& c1, Formula& c2): c1(c1), c2(c2) {}
+    Formula* c1; Formula* c2;
+    Conjunction(Formula* c1, Formula* c2): c1(c1), c2(c2) {}
     bool val() override {
-        return c1 && c2;
+        return c1->val() && c2->val();
     }
 };
 
 struct Equivalence : Formula {
-    Formula& c1; Formula& c2;
-    Equivalence(Formula& c1, Formula& c2): c1(c1), c2(c2) {}
+    Formula* c1; Formula* c2;
+    Equivalence(Formula* c1, Formula* c2): c1(c1), c2(c2) {}
     bool val() override {
-        return (bool)c1 == (bool)c2;
+        return c1->val() == c2->val();
     }
 };
 
 struct Negation : Formula {
-    Formula& c;
-    Negation(Formula& c) : c(c) {}
+    Formula* c;
+    Negation(Formula* c) : c(c) {}
     bool val() override {
-        return !c;
+        return !c->val();
     }
 };
 
@@ -65,17 +65,20 @@ Formula* Parse(char* b, char* e, Formula* vars) {
     if (isletter(*b) and b+1 == e)
         return vars + *b - 'a';
     if (*b=='~')
-        auto* a = new Negation(*);
+        return new Negation(Parse(b+1,e,vars));
+//    if (*b=='('){
+//
+//    }
     return nullptr;
 }
 
 int main() {
     //       r || (p && q)
     bool _p = true, _q = true, _r = false;
-    auto p = Variable(&_p);
-    auto q = Variable(&_q);
-    auto r = Variable(&_r);
-    auto F1 = Conjunction(p,q);
-    auto F2 = Alternative(r,F1);
-    cout << F2.val();
+    auto p = new Variable(&_p);
+    auto q = new Variable(&_q);
+    auto r = new Variable(&_r);
+    auto F1 = new Conjunction(p,q);
+    auto F2 = new Alternative(r,F1);
+    cout << F2->val();
 }
