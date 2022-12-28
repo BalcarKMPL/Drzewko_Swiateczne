ba022fc048d758d051bb2462cecd2322bf05ce13
BalcarKMPL <mazur.jakub05@gmail.com>
2022-12-28 01:04:17 +0100

    working destructors

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 //Jakub Mazur
 #include <iostream>
+#define ifprint true
 using namespace std;
+
+
+void inline print(const char* str){
+    if (ifprint) cout << str << endl;
+}
 
 struct Formula {
     virtual bool val() = 0;
     explicit virtual operator bool() final {
         return val();
     }
+    virtual ~Formula() {};
 };
 
 struct Variable : Formula {
@@ -15,6 +22,7 @@ struct Variable : Formula {
     bool val() override {
         return *ptr;
     }
+    ~Variable() override { print("Variable destructed"); }
 };
 
 struct Implication : Formula {
@@ -23,6 +31,7 @@ struct Implication : Formula {
     bool val() override {
         return !c1->val() || c2->val();
     }
+    ~Implication() override { print("Implication destructed"); delete c1; delete c2; }
 };
 
 struct Alternative : Formula {
@@ -31,6 +40,7 @@ struct Alternative : Formula {
     bool val() override {
         return c1->val() || c2->val();
     }
+    ~Alternative() override { print("Alternative destructed"); delete c1; delete c2;}
 };
 
 struct Conjunction : Formula {
@@ -39,6 +49,7 @@ struct Conjunction : Formula {
     bool val() override {
         return c1->val() && c2->val();
     }
+    ~Conjunction() override { print("Conjunction destructed"); delete c1; delete c2;}
 };
 
 struct Equivalence : Formula {
@@ -47,38 +58,46 @@ struct Equivalence : Formula {
     bool val() override {
         return c1->val() == c2->val();
     }
+    ~Equivalence() override { print("Equivalence destructed"); delete c1; delete c2;}
 };
 
 struct Negation : Formula {
     Formula* c;
-    Negation(Formula* c) : c(c) {}
+    explicit Negation(Formula* c) : c(c) {}
     bool val() override {
         return !c->val();
     }
+    ~Negation() override { print("Negation destructed"); delete c; }
 };
 
 bool isletter(char c) {
-    return 'a'<=c and c <= 'z';
+    return 'a' <= c and c <= 'z';
 }
 
-Formula* Parse(char* b, char* e, Formula* vars) {
-    if (isletter(*b) and b+1 == e)
-        return vars + *b - 'a';
-    if (*b=='~')
-        return new Negation(Parse(b+1,e,vars));
+//Formula* Parse(char* b, char* e, Formula* vars) {
+//    if (isletter(*b) and b+1 == e)
+//        return vars + *b - 'a';
+//    if (*b=='~')
+//        return new Negation(Parse(b+1,e,vars));
 //    if (*b=='('){
 //
 //    }
-    return nullptr;
-}
+//    return nullptr;
+//}
 
 int main() {
-    //       r || (p && q)
-    bool _p = true, _q = true, _r = false;
-    auto p = new Variable(&_p);
-    auto q = new Variable(&_q);
-    auto r = new Variable(&_r);
-    auto F1 = new Conjunction(p,q);
-    auto F2 = new Alternative(r,F1);
-    cout << F2->val();
+//    constexpr int len = 'z' - 'a' + 1;
+//    bool* vars = new bool[len];
+//    Variable* Vars[len];
+//    for (int i = 0; i < len; ++i) {
+//        Vars[i] = new Variable(vars + i);
+//    }
+    bool* b = new bool;
+    *b = true;
+    auto* v = new Variable(b);
+    auto* nv = new Negation(v);
+    auto* nnv = new Negation(nv);
+    cout << v->val() << endl;
+    cout << nv->val() << endl;
+    delete nnv;
 }
